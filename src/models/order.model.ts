45544205374137010a12886e51502f3c06d87c40
Ahmadmoussawi2002 <Ahmadmsw2002@gmail.com>
2025-08-11 00:01:20 +0300

    module-2

---
+++ src/models/order.model.ts
@@ -0,0 +1,26 @@
+import { Item } from "../models/item.model";
+
+export interface IOrder {
+    getItem(): Item;
+    getPrice(): number;
+    getQuantity(): number;
+    getId(): string;
+}
+
+export class Order implements IOrder {
+
+    constructor(private item: Item, private price: number, private quantity: number, private id: string) {}
+
+    getItem(): Item {
+        return this.item;
+    }
+    getPrice(): number {
+        return this.price;
+    }
+    getQuantity(): number {
+        return this.quantity;
+    }
+    getId(): string {
+        return this.id;
+    }
+}
