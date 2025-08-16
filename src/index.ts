f823d8edf42374ab29dfffa9b47f76e16c73356c
Ahmadmoussawi2002 <Ahmadmsw2002@gmail.com>
2025-08-16 21:01:22 +0300

    mdoule_2.3

--- src/index.ts
+++ src/index.ts
@@ -1,71 +1,34 @@
-import path from 'path';
-import {readCsvFile} from './util/parser'
+
+import { CakeBuilder } from "./models/builder/cake.builder";
+
+
 import logger from './util/logger';
-import {readJsonFile} from './util/JsonParser';
-import {readXmlFile} from './util/XMLParser'
 
-const filePath = path.resolve(__dirname, './data/Cake orders.csv');
-const filePath2 = path.resolve(__dirname, './data/book orders.JSON'); // Ensure the path is correct for your environment
-const filePath3 = path.resolve(__dirname, './data/toy orders.XML'); // Ensure the path is correct for your environment
+async function main(){
+  const cakebuiler = new CakeBuilder()
+  const cake = cakebuiler
+    .setType("Birthday Cake")
+    .setFlavor("Chocolate")
+    .setFilling("Strawberry Jam")
+    .setSize(12)
+    .setLayers(3)
+    .setFrostingType("Buttercream")
+    .setFrostingFlavor("Vanilla")
+    .setDecorationType("Sprinkles")
+    .setDecorationColor("Rainbow")
+    .setCustomMessage("Happy Birthday!")
+    .setShape("Round")
+    .setAllergies("None")
+    .setSpecialIngredients("Belgian Chocolate")
+    .setPackagingType("Box")
+    .setPrice(45.99)
+    .setQuantity(1)
+    .build();
 
-export class CsvParser { 
-    async parse() {
-        try {
-       const products = await readCsvFile(filePath)//why i need to set the file path here ? // because this is the file that you want to read from// and what this function do ? // it reads the csv file and returns a 2D array of strings// so i can use it in my code ? // yes, you can use it to get the data from the csv file
-       logger.info(products)
-        return products;
-        } catch (error) {
-            logger.error(error);
-            throw new Error("Error reading CSV file");
-        }
-    }
+console.log(cake);    
 }
 
-
-
-
-
-    
-
-
-export class JsonParser  {
-    async parse() {
-        try {
-            const products = await readJsonFile(filePath2);
-
-           logger.info(products);//here i will get an object
-            return products;
-        } catch (error) {
-            logger.error(error);
-            throw new Error("Error reading JSON file");
-        }
-    }
-}
-
-
-
-export class xmlparser {
-
-    async parse(){
-        try{
-        
-         const rawRecords = await readXmlFile(filePath3);
-
-      
-        // Ensure rawRecords is an array of objects
-            logger.info(rawRecords.data.row);
-        return rawRecords ;
-        
-        }
-        catch(error){
-            throw new Error("Error reading XML file");
-        }
-    }
-}
-
-new xmlparser().parse();
-// new JsonParser().parse();
-// new CsvParser().parse()
+main();
 
 
 
