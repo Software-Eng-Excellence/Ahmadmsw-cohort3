9f2aa55cb6966a513a5744b08dbde84a5f3ee6b1
Ahmadmoussawi2002 <Ahmadmsw2002@gmail.com>
2025-08-22 02:11:31 +0300

    assignment-4

---
+++ src/models/Iorder.model.ts
@@ -0,0 +1,8 @@
+import { Item } from "../models/item.model";
+
+export interface IOrder {
+    getItem(): Item;//here is the bridge between item and order 
+    getPrice(): number;
+    getQuantity(): number;
+   
+}
--- src/models/builder/toy.builder.ts
+++ src/models/builder/toy.builder.ts
@@ -1,15 +1,15 @@
 import { Toy } from "../toy.model";
 
 export class ToyBuilder {
-    private orderID!: number;
-    private type!: string;
-    private ageGroup!: string;
-    private brand!: string;
-    private material!: string;
-    private batteryRequired!: string;
-    private educational!: string;
-    private price!: number;
-    private quantity!: number;
+    private orderID: number = 1;
+    private type: string = "";
+    private ageGroup: string="";
+    private brand: string="";
+    private material: string="";
+    private batteryRequired: string="";
+    private educational: string="";
+    private price: number=0;
+    private quantity: number=0;
 
     setOrderID(orderID: number): ToyBuilder {
         this.orderID = orderID;
@@ -57,24 +57,7 @@ export class ToyBuilder {
     }
 
     build(): Toy {
-        const requiredProperties = [
-            this.orderID,
-            this.type,
-            this.ageGroup,
-            this.brand,
-            this.material,
-            this.batteryRequired,
-            this.educational,
-            this.price,
-            this.quantity
-        ];
-
-        for (const prop of requiredProperties) {
-            if (prop === undefined || prop === null) {
-                throw new Error("Missing required property. Failed to build a toy.");
-            }
-        }
-
+       
         return new Toy(
             this.orderID,
             this.type,
